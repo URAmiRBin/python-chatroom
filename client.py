6d56fdb23ebc2cc2542074cffa3cc5d0f3edb5a0
unknown <ur.amirbin@gmail.com>
2020-11-02 20:30:59 +0330

    client multithread

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 import logging
 from requests.exceptions import HTTPError
+import threading
 
 URL = 'http://127.0.0.1:8080'
 s = requests.Session()
@@ -15,16 +16,38 @@ except:
 else:
     myauth = response.headers['auth'].encode('utf-8')
 
-
-message = ""
-while(message != "!q".encode('utf-8')):
-    message = input(">").encode('utf-8')
-    message_length = str(len(message))
-    try:
-        response = s.post(URL, headers = {'User' : myauth, 'Content-Length' : message_length}, data = message)
-        if response.status_code != 200:
+def client_send():
+    message = ""
+    while(message != "!q".encode('utf-8')):
+        message = input(">").encode('utf-8')
+        message_length = str(len(message))
+        try:
+            response = s.post(URL, headers = {'User' : myauth, 'Content-Length' : message_length}, data = message)
+            if response.status_code != 200:
+                break
+            print(response.text)
+            response.raise_for_status()
+        except Exception as e:
+            print(e)
+            print("ERROR")
             break
-        response.raise_for_status()
-    except:
-        print("ERROR")
-        break
+
+def client_recv():
+    while(True):
+        try:
+            response = s.get(URL, headers = {'User' : myauth})
+            if response.status_code != 200:
+                break
+            print(response.text)
+            response.raise_for_status()
+        except Exception as e:
+            print(e)
+            print("ERROR")
+            break
+
+
+receive_thread = threading.Thread(target=client_recv)
+receive_thread.start()
+
+write_thread = threading.Thread(target=client_send)
+write_thread.start()
